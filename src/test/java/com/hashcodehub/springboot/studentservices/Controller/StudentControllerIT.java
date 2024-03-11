d197afb4a5e3944ffa9b5994e21b4ca91ba7b1f9
sandeepnegi1996 <9041128179sandy@gmail.com>
2024-03-11 11:20:08 +0530

    added two integration tests for StudentController also added pdf complete blog

--- src/test/java/com/hashcodehub/springboot/studentservices/Controller/StudentControllerIT.java
+++ src/test/java/com/hashcodehub/springboot/studentservices/Controller/StudentControllerIT.java
@@ -1,6 +1,7 @@
 package com.hashcodehub.springboot.studentservices.Controller;
 
 
+import com.hashcodehub.springboot.studentservices.Model.Course;
 import com.hashcodehub.springboot.studentservices.StudentServicesApplication;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
@@ -14,15 +15,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-//
-//When we are writing an integration test for a rest service, we would want to launch the entire spring context.
-//
-//@SpringBootTest(classes = StudentServicesApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) : Launch the entire Spring Boot Application on a Random Port
-//@LocalServerPort private int port;: Autowire the random port into the variable so that we can use it create the url.
-//        createURLWithPort(String uri) : Utility method to create the url given an uri. It appends the port.
-//        HttpEntity<String> entity = new HttpEntity<String>(null, headers);: We use entity so that we have the flexibility of adding in request headers in future.
-//        restTemplate.exchange(createURLWithPort("/students/Student1/courses/Course1"),HttpMethod.GET, entity, String.class): Fire a GET request to the specify uri and get the response as a String.
-//        JSONAssert.assertEquals(expected, response.getBody(), false) : Assert that the response contains expected fields.
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = StudentServicesApplication.class,webEnvironment =SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -64,6 +62,33 @@ public class StudentControllerIT {
         JSONAssert.assertEquals(expected,response.getBody(),false);
 
     }
+    
+
+
+    @Test
+    public void addCourse() {
+
+        List<String> stepsList= Arrays.asList("Learn Maven", "Import Project", "First Example", "Second Example");
+
+        Course course = new Course("Course5","Science","10Steps",
+               stepsList );
+
+
+        // with this httpEntity we can pass the required headers and body whatever we wanted to pass
+        HttpEntity<Course> entity = new HttpEntity<>(course,headers);
+
+        ResponseEntity<String> response=restTemplate.exchange(
+                createURLWithPort("/students/Student1/courses"),
+                HttpMethod.POST,entity,String.class);
+
+        String actual= response.getHeaders().get(HttpHeaders.LOCATION).get(0);
+
+        assertTrue(actual.contains("/students/Student1/courses/"));
+
+    }
+
+
+
 
 
 
