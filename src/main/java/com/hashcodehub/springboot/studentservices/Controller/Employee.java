2be95e697986bcc0223dfb4c7c6af7defc1b2f65
sandeepnegi1996 <9041128179sandy@gmail.com>
2024-09-28 12:47:24 +0530

    lombok integration done

---
+++ src/main/java/com/hashcodehub/springboot/studentservices/Controller/Employee.java
@@ -0,0 +1,12 @@
+package com.hashcodehub.springboot.studentservices.Controller;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Employee {
+
+    private int id;
+}
--- src/main/java/com/hashcodehub/springboot/studentservices/StudentServicesApplication.java
+++ src/main/java/com/hashcodehub/springboot/studentservices/StudentServicesApplication.java
@@ -1,13 +1,34 @@
 package com.hashcodehub.springboot.studentservices;
 
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class StudentServicesApplication {
 
+	private static ConfigurableApplicationContext context;
+	private static String[] orginalArg;
 	public static void main(String[] args) {
-		SpringApplication.run(StudentServicesApplication.class, args);
+
+
+
+		context = 	SpringApplication.run(StudentServicesApplication.class, args);
+		orginalArg=args;
+
+	}
+
+	public static void restart() {
+		ApplicationArguments args = context.getBean(ApplicationArguments.class);
+
+		Thread thread = new Thread(() -> {
+			context.close();
+			context = SpringApplication.run(StudentServicesApplication.class, args.getSourceArgs());
+		});
+
+		thread.setDaemon(false);
+		thread.start();
 	}
 
 }
