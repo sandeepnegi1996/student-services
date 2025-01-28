74ab060a9e36f90ab5598f0bff8b7358297f4212
sandeepnegi1996 <9041128179sandy@gmail.com>
2025-01-28 21:50:57 +0530

    created one branch for the feature gcp demo

--- src/main/java/com/hashcodehub/springboot/studentservices/Controller/EmployeeController.java
+++ src/main/java/com/hashcodehub/springboot/studentservices/Controller/EmployeeController.java
@@ -1,10 +1,15 @@
 package com.hashcodehub.springboot.studentservices.Controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
 @RestController
 @RequestMapping("/esg")
 public class EmployeeController {
@@ -13,9 +18,20 @@ public class EmployeeController {
     public Employee getEmployee() {
         System.out.println("inside employee controller get employee");
 
-        Employee employee1 = Employee.builder().id(12).build();
+        log.info("employee controller returns a simple employee");
+        Employee employee1 = Employee.builder().id(1).build();
         return employee1;
+    }
 
+    @GetMapping("/employeList")
+    public List<Employee> getEmployeeList() {
+
+        log.info("fetching the employee list ");
+        Employee e1 = Employee.builder().id(2).build();
+        Employee e2 = Employee.builder().id(3).build();
+
+      List<Employee>  employeeList = Arrays.asList(e1,e2);
+      return  employeeList;
     }
 
 }
--- src/main/java/com/hashcodehub/springboot/studentservices/Controller/IndexController.java
+++ src/main/java/com/hashcodehub/springboot/studentservices/Controller/IndexController.java
@@ -3,6 +3,7 @@ package com.hashcodehub.springboot.studentservices.Controller;
 import com.hashcodehub.springboot.studentservices.helpers.SpringLoggerHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,7 @@ public class IndexController {
         logger.info("this is index info logger message");
         logger.warn("this is index warn logger message");
         logger.error("this is index error logger message");
+        logger.info("GCP Demo");
 
 
         new SpringLoggerHelper().helpMethod();
@@ -34,5 +36,6 @@ public class IndexController {
             logger.error("massive error logger message");
 
         }
+
     }
 }
